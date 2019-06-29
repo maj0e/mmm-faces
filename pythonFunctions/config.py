bea2752c867fa1acc2f67e0efc0da3db228a8b9d
Markus <majoe>
2019-06-29 21:29:30 +0200

    Added Init with json location

--- pythonFunctions/config.py
+++ pythonFunctions/config.py
@@ -15,23 +15,22 @@ import platform
 
 class ConfigHandler:
     _platform = platform.uname()[4]
-    path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    #path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
     # Size (in pixels) to resize images for training and prediction.
     # Don't change this unless you also change the size of the training images.
-    FACE_WIDTH = 92
-    FACE_HEIGHT = 112
+    #FACE_WIDTH = 92
+    #FACE_HEIGHT = 112
 
     # Face detection cascade classifier configuration.
-    # You don't need to modify this unless you know what you're doing.
-    # See: http://docs.opencv.org/modules/objdetect/doc/cascade_classification.html
-    HAAR_FACES = path_to_file + '/haarcascade_frontalface.xml'
+    #HAAR_FACES = path_to_file + '/haarcascade_frontalface.xml'
     
-
-    CONFIG = json.loads(sys.argv[1]);
+    config_storage = None
+    def __init__(config_json):
+        self.config_storage = json.loads(config_json)#(sys.argv[1])
     
     def get(key):
-        return CONFIG[key]
+        return config_storage[key]
     
     def to_node(type, message):
         # convert to json and print (node helper will read from stdout)
@@ -67,25 +66,24 @@ class ConfigHandler:
             to_node("status", "-" * 20)
     
     def get_detector():
-        detectorModel = get("detectorModel")
-        if detectorModel == "HAAR":
+        detectorModel = get("DETECTION_ALGORITHM")
+        if detectorModel == "haar":
             return FaceDetector_HAAR(self.get("HAAR_FACES_LOCATION"), self.get("HAAR_DETECTOR_ARGS")
-        elif detectorModel == "OCV_DNN":
+        elif detectorModel == "ocv_dnn":
             return FaceDetector_DNN(self.get("DNN_DETECTOR_MODEL_LOCATION"), self.get("DNN_DETECTOR_CONFIG_LOCATION")):
-        elif detectorModel == "DLIB_CNN":
+        elif detectorModel == "dlib_cnn":
             return FaceDetector_DLIB_CNN(self.get("DLIB_CNN_MODEL_LOCATION"), self.get("DLIB_CNN_DETECTOR_ARGS"))
         else:
             detector = FaceDetector_HOG()
             detector.function_args = self.get("HOG_DETECTOR_ARGS")
             return detector
         
-    
     def get_recognizer():
-        recognizerModel = ("recognizerModel")
-        if  recognizerModel == "DLIB_CNN":
-            recognizer = FaceRecognizer_DLIB_CNN(self.get("DLIB_RECOGNITION_MODEL_LOCATION"), self.get("FACE_ENCODINGS_LOCATION"))
+        recognizerModel = ("RECOGNITION_ALGORITHM")
+        if  recognizerModel == "dlib_face_encoding":
+            recognizer = FaceRecognizer_DLIB_CNN(self.get("DLIB_RECOGNITION_MODEL_LOCATION"), self.get("FACE_DESCRIPTOR_LOCATION"))
             recognizer.tolerance = self.get("DLIB_RECOGNITION_TOLERANCE")
             return recognizer
         else:
-            return FaceRecognizer_Classic(config.get("trainingFile"))
+            return FaceRecognizer_Classic(config.get("CLASSIC_TRAINING_FILE"))
         
